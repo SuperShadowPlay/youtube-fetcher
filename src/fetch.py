4247f5dc6743c2b250077a7306fd55e401eddf12
SuperShadowPlay <supershadowplay@gmail.com>
2025-04-14 19:48:01 -0400

    Add main function to src/fetch.py to allow for commandline usage

--- src/fetch.py
+++ src/fetch.py
@@ -12,3 +12,15 @@ def fetch(path, songURL, audio_format, options=''):
         else:
             print(f'Problem parsing URL "{songURL}"')
             pass
+
+
+if __name__ == "__main__":
+    # Run as a shell script
+    from sys import argv
+
+    if len(argv) in (3, 4): # Correct amount of args
+        fmt = argv[3] if len(argv) == 4 else "m4a"
+        fetch(argv[2], argv[1], fmt)
+    else:
+        print("Incorrect Usage: python fetch.py <songURL> <path output> [optional format]")
+        
